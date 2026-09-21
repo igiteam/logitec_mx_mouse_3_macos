156e5b8bea453ffcfffec6d6735aa8a0f4364776
Gabriel May <gabrielmay@Gabriels-MacBook-Pro.local>
2026-09-21 10:44:52 +0100

    fixes

--- mx_flow_switch.sh
+++ mx_flow_switch.sh
@@ -91,7 +91,7 @@ cat > "src/MXFlowManager.m" << 'EOF'
 #define CHANNEL_MIN 0
 #define CHANNEL_MAX 2
 
-#define EDGE_THRESHOLD 5
+#define EDGE_THRESHOLD 1
 #define LOGITECH_VID 0x046D
 
 #define HIDPP_REPORT_ID_LONG 0x11
